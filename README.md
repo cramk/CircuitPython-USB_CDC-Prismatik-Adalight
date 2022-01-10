d70576e7902ea86d2834d0eaaad8ce55d7ee08f7
cramk <unknown@unknown>
2022-01-09 21:58:26 -0500

    Add code.py and readme
    -Includes CircuitPython script code.py for Adalight Prismatik control of LEDs through use of CircuitPython USB_CDC
    -Includes readme file for explanation

---
+++ README.md
@@ -0,0 +1,23 @@
+*CircuitPython USB_CDC Ambilight Firmware*
+---------------------
+Control your LED strip over USB running the Adalight Prismatik Software.
+Most compatabile Adalight protocol implementations are written for Arduino, but what if you wanted to control your LEDs with a CircuitPython MicroController?
+
+Prismatik provides numerous quality-of-life features for LED control including screen grabbing, mood lights and API support. Being able to depart from the Arduino-centric setup helps lets additional users enjoy these features and leverage their CircuitPython boards.
+
+
+*Quick Guide*
+---------------------
+- Copy the code.py file to your CIRCUITPYTHON drive
+- Allow for a soft-reboot of your board, or check requirements (enabling USB_CDC data serial will require a hard reboot)
+- Run Prissmatik's configuration wizard for "Adalight" on the correct COM port, baud rate 11520 should work fine
+- Ensure the USB/LED/Software is all running, and enjoy your ambilight experience!
+
+
+
+*Requirements*
+---------------------
+- A USB_CDC supported CircuitPython MicroController (Tested with Tiny2040)
+- Any LED strip, you will have modify the code to drive your specific LED type
+- A CIRCUITPYTHON/boot.py file with USB_CDC Data serial connection enabled
+- Adalight Prismatik software, found at https://lightpack.tv/pages/downloads
---
+++ code.py
@@ -0,0 +1,71 @@
+"""CircuitPython Essentials NeoPixel example"""
+import time
+import board
+import usb_cdc
+import array
+import neopixel
+
+
+pixel_pin = board.GP1
+num_pixels = 60
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1.0, auto_write=False)
+
+if usb_cdc.data is None:
+    print("Need to enable USB CDC serial data in boot.py!")
+    while True:
+        pass
+
+def sendAck():
+    usb_cdc.data.write(bytes("Ada\n",'utf-8'))
+def setup():
+    pixels.fill((100,0,0))
+    pixels.show()
+    time.sleep(0.3)
+    pixels.fill((0,100,0))
+    pixels.show()
+    time.sleep(0.3)
+    pixels.fill((0,0,100))
+    pixels.show()
+    time.sleep(0.3)
+    pixels.fill((0,0,0))
+    pixels.show()
+    time.sleep(0.3)
+    sendAck()
+
+setup()
+serialTimeout = 60000
+usb_cdc.data.timeout = 60 / 1000
+prefix = ['A','d','a']
+lastAckTime = time.monotonic() * 1000
+
+while True:
+
+    i = 0
+    while i < len(prefix):
+        c = usb_cdc.data.read()
+        if(prefix[i] == chr(int.from_bytes(c, "big"))):
+            i += 1
+            continue
+
+        t = time.monotonic() * 1000
+        if t - lastAckTime >= serialTimeout:
+            # send ack if timeout is approaching and we have yet to receive magic
+            sendAck()
+            lastAckTime = t
+        i = 0;
+
+    hi=int.from_bytes(usb_cdc.data.read(), "big")
+    lo=int.from_bytes(usb_cdc.data.read(), "big")
+    chk=int.from_bytes(usb_cdc.data.read(), "big")
+
+    # Cannot XOR bytes, checksum is converted to INT
+    if (chk == (hi ^ lo ^ 0x55)):
+        lastByteTime = t
+        for i in range(0,num_pixels):
+            r = int.from_bytes(usb_cdc.data.read(), "big")
+            g = int.from_bytes(usb_cdc.data.read(), "big")
+            b = int.from_bytes(usb_cdc.data.read(), "big")
+            # neopixel requires INT for color tuple
+            pixels[i] = (r,g,b)
+        pixels.show()
+
